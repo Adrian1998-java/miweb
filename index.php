f21c1c6674f7c2d30b3711fbd039b9a54b4e6ddb
Adrian1998-java <scraggy1998@gmail.com>
2025-06-18 13:48:09 +0100

    Operaciones ariméticas

--- index.php
+++ index.php
@@ -11,6 +11,21 @@
         echo "¡Hola Mundo! Esta es mi primera página web";
         echo "<br>";
         echo "Aquí va otro hola mundo";
-    ?>
+
+        $num1 = 10;
+        $num2 = 2;
+
+        $suma = $num1 + $num2;
+        echo "La suma de $num1 y $num2 es: $suma";
+        
+        $resta = $num1 - $num2;
+        echo "La resta de $num1 y $num2 es: $resta";
+        
+        $multiplicacion = $num1 * $num2;
+        echo "El producto de $num1 y $num2 es: $multiplicacion";
+        
+        $division = $num1 * $num2;
+        echo "La division de $num1 y $num2 es: $division";
+        ?>
 </body>
 </html>
