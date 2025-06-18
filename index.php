2f821d36c8702cd3f4b2c7bbaabf5474b1ab7fa9
Kike-AF <oalviarez2001@gmail.com>
2025-06-18 11:52:12 +0100

    Primer Commit de la clase de Laravel 'Framework PHP'

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Web</title>
+</head>
+<body>
+    <h1>Bienvenido a mi Web</h1>
+    <?php 
+        echo "¡Hola Mundo! Esta es mi primera página web";
+    ?>
+</body>
+</html>
